a866d81d4b7edae11af604bedce9af83f8a2a9eb
Lance Batson <lancebatsondev@gmail.com>
2016-02-17 16:02:33 -0600

    add function for use with redux dev tools chrome extension

--- src/lib/store.js
+++ src/lib/store.js
@@ -3,11 +3,17 @@ import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers';
 
+function reduxDevtools() {
+  return (typeof window === 'object' && typeof window.devToolsExtension !== 'undefined')
+    ? window.devToolsExtension()
+    : f => f;
+}
+
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware,
   createLogger()
 )(createStore);
 
 export default function configureStore (initialState) {
-  return createStoreWithMiddleware(rootReducer, initialState);
+  return createStoreWithMiddleware(rootReducer, initialState, reduxDevtools());
 }
